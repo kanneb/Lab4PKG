84bd3c3beddc99e74f8ba59417e669d34de575e1
Alfred Rosberg <alfro937@student.liu.se>
2026-09-25 09:34:40 +0200

    RC class change. func finished

--- R/linreg.R
+++ R/linreg.R
@@ -1,35 +1,32 @@
-model <- setRefClass(
-  "linreg",
-  fields = list(
-    data = "data.frame",
-    formula = "formula"
-  ),
-  methods = list(
-    initialize = function(formula, data){
-      # Creating X matrix and the dependet variable y
-      X <- model.matrix(formula, data = data)
-      y <- data[[all.vars(formula)[1]]]
 
-      # Regression coefficients
-      beta_hat <- (solve(t(X) %*% X)) %*% (t(X) %*% y)
+  linear_reg <- function(formula, data){
+    # Creating X matrix and the dependet variable y
+    X <- model.matrix(formula, data = data)
+    y <- data[[all.vars(formula)[1]]]
 
-      # Fitted val
-      y_hat <- X %*% beta_hat
+    # Regression coefficients
+    beta_hat <- (solve(t(X) %*% X)) %*% (t(X) %*% y)
 
-      # Residuals
-      e_hat <- y - y_hat
+    # Fitted val
+    y_hat <- X %*% beta_hat
 
-      # Degrees of freedom
-      n <- nrow(X)
-      p <- ncol(X)
+    # Residuals
+    e_hat <- y - y_hat
 
-      df <- n - p
-    }
+    # Degrees of freedom
+    n <- nrow(X)
+    p <- ncol(X)
+    df <- n - p
 
+    # Residual variance
+    sigma_hat2 <- (t(e_hat) %*% e_hat) / df
 
+    # Variance of regression coefficients
+    var_beta_hat <- sigma_hat2 %*% (solve(t(X) %*% X))
 
-
-  )
+    # t-values
+    t_beta <- beta_hat / sqrt(var_beta_hat)
+  }
 )
 
 
